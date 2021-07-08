11cab5eacd45d5cce6b12954273995fc2d3dff4b
luizjurazek <luizjurazek@gmail.com>
2021-07-07 23:14:08 -0300

    mapa

--- mapa.c
+++ mapa.c
@@ -12,7 +12,7 @@ struct cadastro{
 };
 
 // Variaveis
-struct cadastro projeto[2];
+struct cadastro projeto[3];
 int escolha, codigototal;
 
 
@@ -20,7 +20,7 @@ int escolha, codigototal;
 
 // cadastrar projeto
 int cadastrarprojeto(){     
-    int novoprojeto;
+    int cadnovamente;
     printf("----------------------------------------------------------------\n");
     printf("                    Cadastrar projeto: \n");
     projeto[codigototal+1].codigo = codigototal +1;
@@ -29,8 +29,10 @@ int cadastrarprojeto(){
     fflush(stdin);
     gets(projeto[codigototal+1].titulo);
     printf("Informe o status do projeto: \n");
-    scanf("%d", &projeto[codigototal+1].status);
+    fflush(stdin);
+    scanf(" %d", &projeto[codigototal+1].status);
     printf("Informe o ano do projeto: \n");
+    fflush(stdin);
     scanf("%d", &projeto[codigototal+1].ano);
     printf("Qual o gerente do projeto: \n");
     fflush(stdin);
@@ -40,15 +42,27 @@ int cadastrarprojeto(){
     gets(projeto[codigototal+1].descricao);
 
     codigototal = codigototal + 1;
-
+    /*
+    printf("Deseja cadastrar outro projeto: 1 - SIM | 2 - NAO \n");
+    fflush(stdin);
+    scanf("%d", &cadnovamente);
+    if(cadnovamente = 1 && (projeto < 200)){
+        cadastrarprojeto();
+    } else{
+    printf("Retornando ao menu../");
+    }
+    system("pause");
+    return 0; */
 }     
 
 // listar todos projetos
 
 void listarprojetos(){
     int i;
-    printf("----------------------------------------------------------------\n");
-    printf("Projetos: ");
+
+    system("cls");
+    printf("\nProjetos: ");
+    printf("\n-----------------------------------------------");
     for (i = 0; i <= codigototal; i++){
         printf("\nCodigo: %d", projeto[i].codigo);
         printf("\nTitulo: %s", projeto[i].titulo);
@@ -56,25 +70,26 @@ void listarprojetos(){
         printf("\nstatus: %d", projeto[i].status);
         printf("\nAno: %d", projeto[i].ano);
         printf("\nDescricao: %s", projeto[i].descricao);
+        printf("\n-----------------------------------------------\n");
     }
 }
 
 
-
-
 // CORPO PROGRAMA 
-int main(){  
+int main(void){  
     setlocale(LC_ALL, "Portuguese");
     codigototal = 0;
     do {
-        printf("\n---------------------------------------------");
+        printf("\n**********************************************");
         printf("\n          Hub de Projetos - JURAZEK");
-        printf("\n---------------------------------------------");
+        printf("\n**********************************************");
         printf("\n                   MENU");
         printf("\n 1 - Cadastrar um novo projeto ");
         printf("\n 2 - Listar projetos cadastrados");
 
         printf("\n");
+        printf("\n");
+        fflush(stdin);
         scanf("%d", &escolha);
 
         switch (escolha){
